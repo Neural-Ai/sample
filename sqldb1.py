622a416dfd473c5351222b1af88dfa1392f8f22e
Neural-Ai <108761643+Neural-Ai@users.noreply.github.com>
2023-09-15 13:55:38 -0400

    Update sqldb1.py

--- sqldb1.py
+++ sqldb1.py
@@ -1,23 +1,31 @@
-import xlrd
-from sqlite3 import connect
+import pandas as pd
+import sqlite3
 
-# Open the Excel file
-wb = xlrd.open_workbook('alerts_report.xlsx')
-
-# Get the number of sheets in the workbook
-num_sheets = wb.nsheets
-
-# Connect to the SQLite database
-conn = connect('alerts_report.db')
+# Open the SQLite database connection
+conn = sqlite3.connect('your_database.db')  # Replace 'your_database.db' with your desired database file name
 cursor = conn.cursor()
 
-# Loop through each sheet in the workbook
-for i in range(num_sheets):
-    # Read the data from the current sheet
-    sh = wb.sheet_by_index(i)
+# Open the Excel file
+excel_file = pd.ExcelFile('your_file.xlsx')  # Replace with your Excel file
 
-    # Insert the data into the corresponding column of the "Alerts" table
-    cursor.execute("INSERT INTO Alerts VALUES (%s)", [sh.name])
+# Iterate through the sheet names in the Excel file
+for sheet_name in excel_file.sheet_names:
+    # Read the sheet into a DataFrame
+    df = excel_file.parse(sheet_name)
+    
+    # Define a table name for the sheet (you can customize this)
+    table_name = sheet_name.replace(' ', '_')  # Ensure no spaces in table names
+    
+    # Save the DataFrame to the SQLite database
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
 
-# Close the connection to the database
+# Close the SQLite database connection
 conn.close()
+In this modified code, we open the SQLite database connection before iterating through the sheet names and save each sheet as a separate table in the same SQLite database. Finally, we close the connection when we're done saving all the sheets.
+
+Now, all the sheets will be stored as individual tables within the same SQLite database file specified in 'your_database.db'.
+
+
+
+
+
