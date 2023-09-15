32090af3f9398063afe91960802b79e9ad23182e
Neural-Ai <108761643+Neural-Ai@users.noreply.github.com>
2023-09-15 15:49:06 -0400

    Update sqldb1.py

--- sqldb1.py
+++ sqldb1.py
@@ -54,3 +54,31 @@ conn.close()
 
 
 
+import pandas as pd
+import sqlite3
+import datetime
+
+# Step 1: Open the SQLite database connection
+conn = sqlite3.connect('your_database.db')  # Replace 'your_database.db' with your database file name
+cursor = conn.cursor()
+
+# Step 2: Read and save each sheet
+excel_file = pd.ExcelFile('your_file.xlsx')  # Replace with your Excel file
+
+for sheet_name in excel_file.sheet_names:
+    # Step 3: Read the sheet into a DataFrame
+    df = excel_file.parse(sheet_name)
+    
+    # Step 4: Define a table name for the sheet with a unique identifier (e.g., month-year)
+    current_month_year = datetime.datetime.now().strftime('%Y_%m')
+    table_name = f"{sheet_name}_{current_month_year}"
+    
+    # Step 5: Save the DataFrame to the SQLite database (append data if table exists)
+    df.to_sql(table_name, conn, if_exists='append', index=False)
+
+# Step 6: Close the database connection
+conn.close()
+
+
+
+
